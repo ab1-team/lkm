5586c92b1eb03beaacecb5c9e3e9a33c2626af84
nurfandi31 <fandinur241@gmail.com>
2026-06-30 09:35:05 +0700

    fix: perubahan break ke continue

--- resources/views/pelaporan/view/perkembangan_piutang/tunggakan.blade.php
+++ resources/views/pelaporan/view/perkembangan_piutang/tunggakan.blade.php
@@ -9,7 +9,7 @@
     @foreach ($jenis_pp as $jpp)
         @php
             if ($jpp->pinjaman_anggota->isEmpty()) {
-                break;
+                continue;
             }
         @endphp
         @php
